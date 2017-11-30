0ca0ed49c74ace0fd84aadd13adebe6253cc516f
Byron Thomas <byront@humane-it.com>
2017-11-30 13:54:18 +0000

    Slightly more reasonable grid usage in the login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <!-- <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset 3"> -->
-      <div class="col-xs-6 col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-4">
+      <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4 col-md-3 col-md-offset-4">
         <h2>Login</h2>
         <div class="form-group">
           <label for="Email">Email</label>
