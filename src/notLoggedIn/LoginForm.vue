63e56f475f700c7b9452b22af6078c98f96d9d25
Byron Thomas <byront@humane-it.com>
2017-12-04 08:08:47 +0000

    Happy with styling on login form error now

--- src/notLoggedIn/LoginForm.vue
+++ src/notLoggedIn/LoginForm.vue
@@ -3,13 +3,16 @@
     <!-- <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset 3"> -->
     <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4 col-md-3 col-md-offset-4">
       <h2>Login</h2>
-      <div class="form-group">
-        <label for="Email">Email</label>
-        <input type="email" v-model="userDetails.username" class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="Password">Password</label>
-        <input type="password" v-model="userDetails.password" class="form-control">
+      <div v-bind:class="formDivClass">
+        <div class="form-group">
+          <label for="Email">Email</label>
+          <input type="email" v-model="userDetails.username" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="Password">Password</label>
+          <input type="password" v-model="userDetails.password" class="form-control">
+        </div>
+        <p v-if="lastServerError">{{ lastServerError }}</p>
       </div>
       <button 
         class="btn btn-primary" 
@@ -23,20 +26,22 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
 export default {
   data () {
     return { userDetails: {username: '', password: ''} }
+  },
+  computed : {
+    formDivClass() {
+      return {
+        'alert': this.lastServerError,
+        'alert-danger': this.lastServerError
+      }
+    },
+    ...mapGetters(['lastServerError']),
   }
 }
 </script>
 
 <style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,8 @@ export function store(server) {
             notloggedin: notLoggedInStore(server)
         },
         state: {
-            userState: null
+            userState: null,
+            lastServerError: ''
         },
         getters: {
             isLoggedIn: state => {
@@ -21,18 +22,25 @@ export function store(server) {
                 return state.userState == null ?
                     '' : state.userState.username;
             },
+            lastServerError: state => {
+                return state.lastServerError;
+            }
         },
         mutations: {
             setUser(state, payload) {
                 state.userState = payload;
+            },
+            setLastServerError(state, payload) {
+                state.lastServerError = payload;
             }
         },
         actions: {
             authUser({commit}, payload) {
+                commit('setLastServerError', '');
                 server
                     .get('/users', payload)
                     .then(resp => commit('setUser', payload))
-                    .catch(err => alert(err));
+                    .catch(err => commit('setLastServerError', err));
             },
             // TBH: this would have to be handled server-side
             // without client app being involved
