c2f2ae9a93471229745ee3f17e23b346602d27bd
Byron Thomas <byront@humane-it.com>
2017-12-06 12:17:05 +0000

    Minor correction

--- src/loggedIn/homePage/AdSummaryItem.vue
+++ src/loggedIn/homePage/AdSummaryItem.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="row">
       <div class="col-xs-3 col-xs-offset-6">
-        <button class="btn btn-primary" @click="$emit('savePost', ad.id)">View</button>
+        <button class="btn btn-primary" @click="$emit('viewPost', ad.id)">View</button>
       </div>
       <div class="col-xs-3">
         <button class="btn btn-primary" @click="$emit('savePost', ad.id)">Save</button>
