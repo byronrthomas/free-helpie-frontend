7ec399b0f719890355aaec8a9bcbee7e2bf0a436
Byron Thomas <byront@humane-it.com>
2017-12-07 12:08:53 +0000

    Latest ads basically styled OK now

--- src/loggedIn/homePage/AdSummaryItem.vue
+++ src/loggedIn/homePage/AdSummaryItem.vue
@@ -2,18 +2,20 @@
   <div class="ad-summary-container">
     <div class="row">
       <div class="col-xs-3">
-        <a href="unknown">{{ ad.postedBy }}</a>
-      </div>
-      <div class="col-xs-3">{{ ad.title }} </div>
-      <div class="col-xs-3">{{ formattedSkills }}</div>
-      <div class="col-xs-3">{{ ad.location }}</div>
-    </div>
-    <div class="row">
-      <div class="col-xs-3 col-xs-offset-6">
-        <button class="btn btn-primary" @click="$emit('viewPost')">View</button>
+        <a href="unknown" @click.prevent="alertNotImplemented">{{ ad.postedBy }}</a>
       </div>
       <div class="col-xs-3">
-        <button class="btn btn-primary" @click="$emit('savePost')">Save</button>
+        <a href="unknown" @click.prevent="$emit('viewPost')">
+          {{ ad.title }}
+        </a>
+      </div>
+      <div class="col-xs-3">{{ formattedSkills }}</div>
+      <div class="col-xs-2">{{ ad.location }}</div>
+      <div class="col-xs-1">
+        <button class="btn btn-default save-button" @click="$emit('savePost')">
+          <img src='../../assets/starInactive.png' alt="Unsave" :hidden="isSaved"  style="width=14px;height=14px" width="36px" height="36px">
+          <img src='../../assets/starActive.png' alt="Save" style="width=14px;height=14px" :hidden="!isSaved"  width="36px" height="36px">
+        </button>
       </div>
     </div>
   </div>
@@ -21,16 +23,40 @@
 
 <script>
 export default {
-  props: ['ad'],
+  props: ['ad', 'isSaved'],
   computed: {
     formattedSkills () {
       return this.ad.skills.length === 1
         ? this.ad.skills[0] 
         : this.ad.skills.reduce( (acc, nxt) => acc + ", " + nxt, '')
+    },
+    saveOrUnsave () {
+      return this.isSaved ? 'Unsave' : 'Save'
+    },
+    starImagePath () {
+      return '../../assets/' + (this.isSaved ? 'starActive.png' : 'starInactive.png')
+    }
+  },
+  methods: {
+    alertNotImplemented() {
+      alert("Don't know where this link would go to yet? User profile?")
     }
   }
 }
 </script>
 
 <style>
+  .ad-summary-container {
+      background-color: white;
+      border-width: 1px 1px 1px 1px; 
+      border-color: #ddd; 
+      border-style: solid;
+      padding: 10px;
+      margin-bottom: 10px;
+  }
+  .save-button {
+    margin-top: -10px;
+    margin-bottom: -10px;
+    border-style: hidden;
+  }
 </style>
--- src/loggedIn/homePage/LatestAdsPage.vue
+++ src/loggedIn/homePage/LatestAdsPage.vue
@@ -1,20 +1,18 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col-xs-6 col-xs-offset-6">
+  <div class="latest-ad-container">
+    <div class="row" style="margin-bottom: 10px">
+      <div class="col-xs-12">
+        <span>Filter results</span>
         <button 
-          class="btn btn-primary" 
-          @click="currentFilter = 'Unfiltered'">
-          Unfiltered
-        </button>
-        <button 
-          class="btn btn-primary"
-          @click="currentFilter = 'MySkills'">
+          class="btn"
+          @click="filteringBySkills = !filteringBySkills"
+          :class="{'btn-primary': filteringBySkills}">
           My skills
         </button>
         <button 
-          class="btn btn-primary" 
-          @click="currentFilter = 'MyLocation'">
+          class="btn" 
+          @click="filteringByLocation = !filteringByLocation"
+          :class="{'btn-primary': filteringByLocation}">
           My location
         </button>
       </div>
@@ -25,6 +23,7 @@
           v-for="post in filteredPosts" 
           :ad="post" 
           :key="post.id"
+          :is-saved="post.postedBy === 'John Doe'"
           @viewPost="$emit('viewPost', post)"
           @savePost="$emit('savePost', post)" />
       </div>
@@ -38,19 +37,27 @@ import AdSummaryItem from './AdSummaryItem.vue'
 
 export default {
   data() {
-    return {currentFilter: 'Unfiltered', posts: INITIAL_POSTS}
+    return {
+      filteringBySkills: false,
+      filteringByLocation: false,
+      posts: INITIAL_POSTS}
   },
   computed: {
     filteredPosts() {
       console.log("Attempting to filter result")
-      const result = (!this.currentFilter || this.currentFilter === 'Unfiltered')
-        ? this.posts
-        : (this.currentFilter === "MySkills"
-            ? this.posts.filter(post => post.skills.includes('Executive coaching'))
-            : this.posts.filter(post => post.remote || post.location === 'North-west London'))
+      let result = this.posts
+      result = this.filteringBySkills
+        ? result.filter(post => post.skills.includes('Executive coaching'))
+        : result
+      result = this.filteringByLocation
+        ? result.filter(post => post.remote || post.location === 'North-west London')
+        : result
       console.log("Filtered to:")
       console.log(result)
       return result;
+    },
+    skillsFilterActive() {
+      return this.currentFilter === "MySkills" || this.currentFilter === "Both"
     }
   },
   components: {
@@ -60,4 +67,11 @@ export default {
 </script>
 
 <style>
+  .latest-ad-container {
+    border-style: solid;
+    border-width: 1px 1px 1px 1px; 
+    border-color: #ddd; 
+    padding: 10px;
+    margin-top: 10px;
+  }
 </style>
