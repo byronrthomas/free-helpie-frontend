317157d2ff1a9dcdd0020fdb80b5c60b0b701ddd
Byron Thomas <byront@humane-it.com>
2017-12-13 10:01:13 +0000

    Correct one more ads -> posts

--- src/loggedIn/homePage/routes.js
+++ src/loggedIn/homePage/routes.js
@@ -1,6 +1,6 @@
 import LatestPostsPage from './LatestPostsPage.vue'
 import PostDetailPage from './PostDetailPage.vue'
-import SavedAdsPage from './SavedAdsPage.vue'
+import SavedPostsPage from './SavedPostsPage.vue'
 import MailboxPage from './MailboxPage.vue'
 import MailDetailPage from './MailDetailPage.vue'
 import YourPostsPage from './YourPostsPage.vue'
@@ -8,7 +8,7 @@ import YourPostsPage from './YourPostsPage.vue'
 export const HOME_COMPONENT_ROUTES = [
   {path: 'latestPosts', component: LatestPostsPage, name: 'latestPosts'},
   {path: 'postDetail/:postId', component: PostDetailPage, name: 'postDetail', props: true},
-  {path: 'savedPosts', component: SavedAdsPage, name: 'savedPosts'},
+  {path: 'savedPosts', component: SavedPostsPage, name: 'savedPosts'},
   {path: 'yourPosts', component: YourPostsPage, name: 'yourPosts'},
   {path: 'mailbox', component: MailboxPage, name: 'mailbox'},
   {path: 'mailDetail/:mailId', component: MailDetailPage, name: 'mailDetail', props: true}
