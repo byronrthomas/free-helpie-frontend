63dfaa0ac1eeaf2ca6120085f24c5bad0b51fe62
Byron Thomas <byront@humane-it.com>
2017-12-31 16:04:04 +0000

    Rename page to something more useful

--- src/loggedIn/routes.js
+++ src/loggedIn/routes.js
@@ -1,10 +1,10 @@
 import {HOME_COMPONENT_ROUTES} from './homePage/routes'
 
-import HelperProfile from './HelperProfile.vue'
+import EditProfilePage from './EditProfilePage.vue'
 import HomePage from './HomePage.vue'
 import AccountPage from './AccountPage.vue'
 
 export const LOGGED_IN_ROUTES = [
-  {path: 'profile', component: HelperProfile, name: 'profile'},
+  {path: 'profile', component: EditProfilePage, name: 'profile'},
   {path: 'account', component: AccountPage, name: 'account'},
   {path: 'home', component: HomePage, name: 'home', children: HOME_COMPONENT_ROUTES}]
