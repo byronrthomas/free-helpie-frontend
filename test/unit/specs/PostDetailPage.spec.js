8ea60d32611766886c6a5a69534575213204aeb7
Byron Thomas <byront@humane-it.com>
2018-01-03 15:34:55 +0000

    Fix warnings on post detail page test

--- test/unit/specs/PostDetailPage.spec.js
+++ test/unit/specs/PostDetailPage.spec.js
@@ -15,7 +15,14 @@ const TEST_USER_ID = 777
 const emptyPost = {
   skills: [],
   locations: [],
-  interests: []
+  interests: [],
+  timings: {
+    regularAmount: {
+      unit: '',
+      frequency: ''
+    },
+    slots: []
+  }
 }
 
 const currentUsersPostId = 1
@@ -123,7 +130,7 @@ describe('PostDetailsPage', () => {
   })
 
   it('should dispatch getMailThread(thread_id, sent_date ASC) if post.postedBy !== currentUser', () => {
-    shallow(PostDetailPage, {localVue, store, propsData: {postId: `${otherUserPostId}`}})
+    shallow(PostDetailPage, {localVue, store, propsData: {postId: otherUserPostId}})
 
     const expectedActionData = {
       relatedToPostId: otherUserPostId,
@@ -136,14 +143,14 @@ describe('PostDetailsPage', () => {
   })
 
   it('should display a link to the mailbox if post.postedBy === currentUser', () => {
-    const onTest = mount(PostDetailPage, {localVue, store, propsData: {postId: `${currentUsersPostId}`}})
+    const onTest = mount(PostDetailPage, {localVue, store, propsData: {postId: currentUsersPostId}})
     expect(onTest.vm.postedByCurrentUser).toBe(true)
 
     expect(onTest.contains('#linkToMailbox')).toBe(true)
   })
 
   it('should dispatch newEmail(post_id, post_author, thread_author, msg_text) if it receives a sendEmail event', () => {
-    const onTest = shallow(PostDetailPage, {localVue, store, propsData: {postId: `${otherUserPostId}`}})
+    const onTest = shallow(PostDetailPage, {localVue, store, propsData: {postId: otherUserPostId}})
 
     const emailText = 'This is a test email'
     // Ideally we would trigger the event from the MTC, rather than call the method directly
@@ -159,20 +166,20 @@ describe('PostDetailsPage', () => {
   })
 
   it('should pass on mails in the thread to its MailThreadContainer component', () => {
-    const onTest = mount(PostDetailPage, {localVue, store, propsData: {postId: `${otherUserPostId}`}})
+    const onTest = mount(PostDetailPage, {localVue, store, propsData: {postId: otherUserPostId}})
     
     expect(onTest.find(MailThreadContainer).vm.mailItems).toEqual(testEmails)
   })
 
   it('should pass cancelConnectState as disabled to its MailThreadContainer component if there are no emails', () => {
     const noEmailsStore = new Vuex.Store(storeConfig([], actions))
-    const onTest = mount(PostDetailPage, {localVue, store: noEmailsStore, propsData: {postId: `${otherUserPostId}`}})
+    const onTest = mount(PostDetailPage, {localVue, store: noEmailsStore, propsData: {postId: otherUserPostId}})
     
     expect(onTest.find(MailThreadContainer).vm.connectOrCancelAllowed).toEqual('disabled')
   })
 
   it('should pass cancelConnectState as connectAllowed to its MailThreadContainer component if there are no emails', () => {
-    const onTest = mount(PostDetailPage, {localVue, store, propsData: {postId: `${otherUserPostId}`}})
+    const onTest = mount(PostDetailPage, {localVue, store, propsData: {postId: otherUserPostId}})
     
     expect(onTest.find(MailThreadContainer).vm.connectOrCancelAllowed).toEqual('connectAllowed')
   })
