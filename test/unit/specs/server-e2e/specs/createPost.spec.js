7074ae5581853b46d7748e14c054a69f691ee4d5
Byron Thomas <byront@humane-it.com>
2018-01-15 18:15:20 +0000

    Comment

--- test/unit/specs/server-e2e/specs/createPost.spec.js
+++ test/unit/specs/server-e2e/specs/createPost.spec.js
@@ -4,6 +4,8 @@ import { postFix } from '../fixtures/post.fix'
 import { accountLib } from '../lib/account.lib'
 import { postLib } from '../lib/post.lib'
 
+// TODO: put / delete / etc and other forms of filtering not yet covered
+
 describe('Create post', () => {
   const state = {}
   initServer(state)
